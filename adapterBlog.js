cd130138fcc1a42a71e0985d9ba67a43c0291808
Frank Hinkel <frank.hinkel@innoq.com>
2014-01-29 19:22:29 +0100

    beautyfied

--- adapterBlog.js
+++ adapterBlog.js
@@ -1,32 +1,27 @@
 var request = require("request");
-var jsonpath = require('JSONPath').eval;
 var cheerio = require('cheerio');
 
 var auth = {
-  'auth': {
-    'user': 'usr',
-    'pass': 'pw',
-    'sendImmediately': false
+	'auth': {
+		'user': 'TODO',
+		'pass': 'TODO',
+		'sendImmediately': false
 	}
 }
 
 module.exports = function (query, callback) {
-	request.get('url' + query, auth, function (error, response, body) {
-	  if (!error && response.statusCode == 200) {
-
-	  	$ = cheerio.load(body);
-
-	  	var results = [];
-
-	  	var articles = $('article').children('h2').each(function(i, elem){
-	  		var href = $(this).children('a').attr('href');
-		 	var textline = $(this).children('a').text();
-			var result = {title: textline, url: href}
-		 	results.push(result);
-	  	});
-
-	    callback(null, results);
-	  }
+	request.get('TODO URL' + query, auth, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+	  		$ = cheerio.load(body);
+	  		var results = [];
+	  		var articles = $('article').children('h2').each(function(i, elem){
+	  			var href = $(this).children('a').attr('href');
+				var textline = $(this).children('a').text();
+				var result = {title: textline, url: href}
+				results.push(result);
+	  		});
+	    	callback(null, results);
+		}
 	})
 };
 
