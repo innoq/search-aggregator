4f9ca2b3412295e5585545abb27963ba073d370f
Bastian Krol <bastian.krol@web.de>
2014-01-29 16:36:12 +0100

    add adapter for github issue search

---
+++ adapter_github_issues.js
@@ -0,0 +1,36 @@
+var request = require('request');
+
+// All github search APIs have a rate limit of 5 requests per minute for
+// unauthicated requests. Providing basic auth or OAuth credentials would
+// definitely help, raising the rate limit to 20 requests per minute.
+
+module.exports = function (query, callback) {
+
+    // TODO Restrict issue search to user innoQ
+
+    // More optional query parameters
+    // page:
+    // per_page:
+    // sort:
+    // order:
+
+    var options = {
+        url: 'https://api.github.com/search/issues?q=' + query,
+        headers: {
+            'User-Agent': 'search-aggregator',
+        }
+    };
+	  request.get(options, function (error, response, body) {
+        console.log(body);
+	      if (error) {
+            console.log('error: ');
+            console.log(error);
+	          return callback(error);
+	      } else if (response.statusCode != 200) {
+            console.log('error: ');
+            console.log(error);
+	          return callback(new Error('Unexpected HTTP status: ' + response.status));
+        }
+        return callback(null, body);
+    });
+};
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var async = require("async");
 var adapterBlog = require("./adapterBlog");
+var adapterGithubIssues = require("./adapter_github_issues");
 
-var ADAPTORS = [dummyAdaptor, adapterBlog];
+var ADAPTORS = [dummyAdaptor, adapterGithubIssues] //, adapterBlog];
 
 var app = express();
 
@@ -12,6 +13,13 @@ app.get("/", function (req, res, next) {
 
 	// TODO: error handling (timeout?)
 	async.parallel(tasks, function(err, results) {
+    if (err) {
+      console.log('Error in aggregation: ');
+      console.log(err);
+		  res.writeHead(500, { "Content-Type": "text/plain" });
+		  res.end('I fucked up. Sorry.');
+      return;
+    }
 		res.writeHead(200, { "Content-Type": "application/json" });
 		res.end(JSON.stringify(results));
 	});
